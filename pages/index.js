74164d8b23de4eb55583c6ee4ed9cc178f02ab03
Hrvoje Mlinarevic <hrvoje.mlinarevic@gmail.com>
2022-01-16 22:41:53 +0100

    feat: add lorem paragraph

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,11 @@ export default function Home() {
 	return (
 		<div>
 			<h1>Hello World!</h1>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus,
+				recusandae? Quos qui eligendi, fugit officia inventore sed. Maiores,
+				dolorem culpa.
+			</p>
 		</div>
 	);
 }
